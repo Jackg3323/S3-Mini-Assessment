f456afd2716da3a6fdb592dc5956017e6e51a7d9
Jack Guthrie <jackg3323@gmail.com>
2022-09-02 13:02:01 +0100

    Added table selection

--- test/specs/rubric.e2e.js
+++ test/specs/rubric.e2e.js
@@ -1,9 +1,18 @@
-import Rubric from "../pageobjects/rubric.page";
-
 describe("My Login application", function () {
   it("should login with valid credentials", async function () {
-    await browser.open("https://inspiring-wisp-f3b8d3.netlify.app");
+    await browser.url("https://inspiring-wisp-f3b8d3.netlify.app");
 
-    await Rubric.login("tomsmith", "SuperSecretPassword!");
+    const row1 = await $("//tbody/tr[1]/td[1]");
+    const row2 = await $("//tbody/tr[2]/td[1]");
+    const row3 = await $("//tbody/tr[3]/td[1]");
+
+    await row1.click();
+    await row2.click();
+    await row3.click();
+
+    const score = await $("#score");
+    const percent = await $("#percent");
+    expect(score).toContain("30");
+    expect(percent).toContain("100");
   });
 });
